377e89e60119203004d02ac1a1bc90193e0b051e
NguyenBao02 <nguyenquocbao022003@gmail.com>
2024-04-10 21:56:52 +0700

    component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>My Frist Component</div>
+        );
+    }
+}
+
+export default MyComponent;
