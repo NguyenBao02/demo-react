15e66f49a091c6b605c99db7c20037bfc0791c87
NguyenBao02 <nguyenquocbao022003@gmail.com>
2024-04-12 14:47:25 +0700

    update

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,34 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+    state = {
+        listUsers :  [
+            {
+                id      : 1,
+                name    : 'Hoi Dan IT',
+                age     : 26
+            },
+            {
+                id      : 2,
+                name    : 'Bao',
+                age     : 21
+            },
+            {
+                id      : 3,
+                name    : 'Quyen',
+                age     : 22
+            },
+        ]
+    }
+
     render() {
         return (
             <div>
                 <UserInfor />
+                <br />
+                <DisplayInfor listUsers={this.state.listUsers}/>
             </div>
         );
     }
