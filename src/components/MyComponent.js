83b61cf90b81793e461a2ab0dfa9e685427b0757
NguyenBao02 <nguyenquocbao022003@gmail.com>
2024-04-12 08:38:36 +0700

    event dom

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,16 @@ class MyComponent extends React.Component {
         age     : 21
     };
 
+    handleClick() {
+        console.log('My name is');
+    }
+
     //JSX
     render() {
         return (
             <div>
                 My name is {this.state.name}
+                <button onClick={this.handleClick}>Click Me</button>
             </div>
         );
     }
